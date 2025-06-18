560e422087455ad52518b36dc86b9eb30001e572
David Elias Sandoval Bernitta <d4vidsandoval@gmail.com>
2025-06-18 15:55:43 +0000

    chore: adding grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,42 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { Grid } from '@mui/material';
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Bienvenido al dashboard 🔥</h1>
+      <Grid container spacing={5} justifyContent="center" alignItems="center">
+        {/* Encabezado */}
+        <Grid size={{ xs: 12, md: 12 }}>Elemento: Encabezado</Grid>
+
+        {/* Alertas */}
+        <Grid size={{ xs: 12, md: 12 }}>Elemento: Alertas</Grid>
+
+        {/* Selector */}
+        <Grid size={{ xs: 12, md: 3 }}>Elemento: Selector</Grid>
+
+        {/* Indicadores */}
+        <Grid size={{ xs: 12, md: 9 }}>Elemento: Indicadores</Grid>
+
+        {/* Gráfico */}
+        <Grid size={{ xs: 12, md: 6 }} sx={{ display: { xs: 'none', md: 'block' } }}>
+          Elemento: Gráfico
+        </Grid>
+
+        {/* Tabla */}
+        <Grid size={{ xs: 12, md: 6 }} sx={{ display: { xs: 'none', md: 'block' } }}>
+          Elemento: Tabla
+        </Grid>
+
+        {/* Información adicional */}
+        <Grid size={{ xs: 12, md: 12 }}>Elemento: Información adicional</Grid>
+      </Grid>
+    </div>
   )
 }
 
