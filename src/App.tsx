eb414d043012f286f09ce9bcbeddd2f7aff13152
David Elias Sandoval Bernitta <d4vidsandoval@gmail.com>
2025-07-14 15:29:27 +0000

    chore: pwa

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
     <>
       <Grid container spacing={5} justifyContent="center" alignItems="center">
         {/* Encabezado */}
-        <Grid size={{ xs: 12, md: 12 }} container justifyContent="left" alignItems="center" padding={5} spacing={2} boxShadow={"0 2px 6px rgba(0,0,0,0.1)"} borderRadius={3} sx={{background: "linear-gradient(90deg, #4A7BD3 0%, #3255A2 50%, #1A2C6B 100%)"}}>
+        <Grid size={{ xs: 12, md: 12 }} container justifyContent="left" alignItems="center" padding={4} spacing={2} boxShadow={"0 2px 6px rgba(0,0,0,0.1)"} borderRadius={3} sx={{background: "linear-gradient(90deg, #4A7BD3 0%, #3255A2 50%, #1A2C6B 100%)"}}>
           <HeaderUI />
         </Grid>
 
--- src/components/HeaderUI.tsx
+++ src/components/HeaderUI.tsx
@@ -3,11 +3,16 @@ import { CloudSun } from 'lucide-react';
 
 export default function HeaderUI() {
     return (
-        <>
-            <CloudSun color="#fff" size={45} />
-            <Typography variant="h4" component="h1" fontWeight="bold" color='#fff'>
-                Dashboard de Clima
-            </Typography>
-        </>
+        <div style={{ display: 'flex', gap: '30px', paddingLeft: '15px' }}>
+            <CloudSun color="#fff" size={70} />
+            <div style={{ textAlign: 'left' }}>
+                <Typography variant="h4" component="h1" fontWeight="bold" color='#fff'>
+                    Dashboard de Clima
+                </Typography>
+                <Typography variant="h6" component="h2" color='#fff'>
+                    Información meteorológica actualizada
+                </Typography>
+            </div>
+        </div>
     )
 }
