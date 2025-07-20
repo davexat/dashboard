6c21cd297ca7587cb29a4faff9bf83fbec18d3ec
Bruno Romero <152760572+Brun025-5@users.noreply.github.com>
2025-07-20 01:31:34 +0000

    chore: modifying chart

--- src/components/ChartUI.tsx
+++ src/components/ChartUI.tsx
@@ -10,27 +10,36 @@ interface ChartUIProps {
 }
 
 export default function ChartUI({ loading, error, labels, values1, values2 }: ChartUIProps) {
+
   if (loading) return <Typography>Cargando gráfico...</Typography>;
   if (error) return <Typography color="error">Error: {error}</Typography>;
   if (!labels.length || !values1.length || !values2.length) return <Typography>No hay datos para mostrar.</Typography>;
 
   return (
     <>
-      <Typography variant="h5" component="div">
+      <Typography variant="h5" component="div" sx={{
+        color: '#000'
+      }}>
         Temperatura y velocidad del viento por hora
       </Typography>
       <LineChart
         height={300}
         series={[
-          { data: values1, label: 'Temperatura (°C)', showMark:false },
-          { data: values2, label: 'Velocidad viento (km/h)', showMark:false },
+          { data: values1, label: 'Temperatura (°C)', showMark: false },
+          { data: values2, label: 'Velocidad viento (km/h)', showMark: false },
         ]}
         xAxis={[
           {
             scaleType: 'point',
             data: labels,
-            tickLabelInterval: (_, i) => i % 12 === 0, // Muestra una etiqueta cada 6 puntos
-            valueFormatter: (v: string) => v.slice(11, 16), // Solo hora y minutos
+            tickInterval: (value, _) => {
+              // value es el string de la hora, por ejemplo "2024-07-20T00:00"
+              // Solo muestra tick si la hora es "00:00"
+              return value.slice(11, 16) === "00:00";
+            },
+            valueFormatter: (v: string, context?: { location?: string }) =>
+              context?.location === 'tooltip' ? v.slice(11, 16) : v.slice(0, 10),
+
           }
         ]}
       />
