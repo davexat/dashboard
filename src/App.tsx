616412570809452edcf0f8b343650b7dde0269fe
davexat <yisuscristh20@gmail.com>
2025-07-21 17:45:27 -0500

    chore: indicator name file

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import HeaderUI from './components/HeaderUI';
 import AlertUI from './components/AlertUI';
 import LocationSelectorUI from './components/LocationSelectorUI';
 import IndicatorUI from './components/IndicatorUI';
-import Indicator2UI from './components/Indicator2UI';
 import DataFetcher from './functions/DataFetcher';
 import TableUI from './components/TableUI';
 import ChartUI from './components/ChartUI';
@@ -43,41 +42,13 @@ function App() {
           <LocationSelectorUI onLocationSelect={setCity}/>
         </Grid>
 
-        <Grid container size={{ xs: 12, md: 9 }} >
-          <Indicator2UI data={dataFetcherOutput}></Indicator2UI>
-        </Grid>
-
         {/* Indicadores */}
         <Grid container size={{ xs: 12, md: 9 }} >
-
           {dataFetcherOutput.loading && <p>Cargando datos...</p>}
           {dataFetcherOutput.error && <p>Error: {dataFetcherOutput.error}</p>}
           {dataFetcherOutput.data && (
             <>
-              <Grid size={{ xs: 12, md: 3 }} >
-                <IndicatorUI
-                  title='Temperatura (2m)'
-                  description={dataFetcherOutput.data.current.temperature_2m + " " + dataFetcherOutput.data.current_units.temperature_2m} />
-              </Grid>
-
-              <Grid size={{ xs: 12, md: 3 }}>
-                <IndicatorUI
-                  title='Temperatura aparente'
-                  description={dataFetcherOutput.data.current.apparent_temperature + " " + dataFetcherOutput.data.current_units.apparent_temperature} />
-              </Grid>
-
-              <Grid size={{ xs: 12, md: 3 }}>
-                <IndicatorUI
-                  title='Velocidad del viento'
-                  description={dataFetcherOutput.data.current.wind_speed_10m + " " + dataFetcherOutput.data.current_units.wind_speed_10m} />
-              </Grid>
-
-              <Grid size={{ xs: 12, md: 3 }}>
-                <IndicatorUI
-                  title='Humedad relativa'
-                  description={dataFetcherOutput.data.current.relative_humidity_2m + " " + dataFetcherOutput.data.current_units.relative_humidity_2m} />
-              </Grid>
-
+              <IndicatorUI data={dataFetcherOutput} />
             </>
           )}
         </Grid>
@@ -111,7 +82,6 @@ function App() {
             weatherInfo={dataFetcherOutput.data?.hourly}
           />
         </Grid>
-
       </Grid>
     </>
   )
